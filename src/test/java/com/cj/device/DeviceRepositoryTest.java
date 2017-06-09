ef32af945758121e9a0a69a221b5de488e5c1b73
christopheralan88 <christopheralan88@gmail.com>
2017-06-09 11:21:19 -0400

    started adding unit testing

---
+++ src/test/java/com/cj/device/DeviceRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.cj.device;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+
+public class DeviceRepositoryTest {
+    @Autowired
+    private DeviceRepository devices;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void findAllReturnsDevices() throws Exception {
+        //Act
+        //Iterable<Device> d = devices.findAll();
+        Page<Device> allDevices = devices.findAll(new PageRequest(1, 20));
+
+        //Assert
+        Assert.assertTrue(allDevices.getTotalElements() > 0);
+    }
+
+}
